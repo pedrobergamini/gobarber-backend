a85bd68ae38b1f90a34e6886e27c561c932eab97
Pedro Bergamini <pedrobergamini@MacBook-Pro-de-Pedro.local>
2019-09-08 18:02:16 -0300

    mail features

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 import User from '../models/User';
 import File from '../models/File';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -120,7 +121,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -140,7 +154,20 @@ class AppointmentController {
 
     await appointment.save();
 
-    return res.json();
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
+
+    return res.json(appointment);
   }
 }
 
