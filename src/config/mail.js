95627a4b090dc56cb918c9cbae563245a0d32bf0
Pedro Bergamini <pedrobergamini@MacBook-Pro-de-Pedro.local>
2019-09-10 11:41:03 -0300

    finished nodejs 03

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '5124f9af172268',
-    pass: 'eefdf398bc6184',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://b6c751c3effe4b8085bcabec11da9ccc@sentry.io/1721539',
+  dsn: process.env.SENTRY_DSN,
 };
